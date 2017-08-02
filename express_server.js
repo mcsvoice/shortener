637c062de2c314c8781c5cdf3b3b0eda85ea142b
MCSV <michael.cawston.stewart@gmail.com>
2017-08-02 06:39:13 +0000

    cleaned up some older files.

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-//var PORT = process.env.PORT || 8080; // default port 8080
+//const PORT = process.env.PORT || 8080; // default port 8080
 const handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 const bodyParser = require("body-parser");
